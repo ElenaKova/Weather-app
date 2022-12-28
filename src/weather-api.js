a12b001da56ed2baaa15cf9e52bda84df4619b57
EleKo <ikovalenko1999@gmail.com>
2022-12-28 14:19:36 +0100

    fixed container size

--- src/weather-api.js
+++ src/weather-api.js
@@ -1,10 +1,9 @@
-
 function displayWeather(response) {
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#temperature").innerHTML = Math.round(
         response.data.main.temp
     );
-
+    document.querySelector("#precipitation").innerHTML = response.data.main.precipitation;
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind").innerHTML = Math.round(
         response.data.wind.speed
