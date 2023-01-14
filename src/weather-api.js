fa7ee151c437e245e49c8e09937390b3ea0a7909
EleKo <ikovalenko1999@gmail.com>
2023-01-15 00:04:59 +0100

    fixed forecast for week

--- src/weather-api.js
+++ src/weather-api.js
@@ -1,26 +1,74 @@
 // import axios from "axios";
 
+function formatDate(date) {
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let dayIndex = date.getDay();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[dayIndex];
+
+    return `${day} ${hours}:${minutes}`;
+}
+
+let currentDayTime = document.querySelector("#day-time");
+let time = new Date();
+currentDayTime.innerHTML = formatDate(time);
+
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat"
+    ]
+
+    return days[day];
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = "";
-    let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
-    days.forEach(function (day) {
-        forecastHTML = forecastHTML +
-            `
+    // let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
+    forecast.forEach(function (forecastDay, idx) {
+        if (idx < 7) {
+            forecastHTML = forecastHTML +
+                `
         <div class="box-day container mx-1 ">
-            <div class = "day-of-week align-items-center" > ${day}</div>
-                <img src="http://openweathermap.org/img/wn/50d@2x.png"
+            <div class = "day-of-week align-items-center" > ${formatDay(forecastDay.dt)}</div>
+
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
                     alt=""
                     width="42"/>
                 <div class="weather-forecast-temp">
-                        <span class="weather-forecast-temp-max">18˚</span>
-                        <span class = "weather-forecast-temp-min" > 13˚ </span>
+                        <span class = "weather-forecast-temp-max">${Math.round(forecastDay.temp.max)}˚</span>
+                        <span class = "weather-forecast-temp-min">${Math.round(forecastDay.temp.min)}˚</span>
                 </div>
             </div>
         </div>
     `;
+        }
     })
 
     // forecastHTML = forecastHTML + `</div>`
